8dca1f4ecba65f110928d3ee62b92cc580ffe072
stanislavmyname <115893292+stanislavmyname@users.noreply.github.com>
2023-06-26 11:58:07 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,54 @@
-function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+// Задание 1
+function getArrayParams(arr) {
+  let max = arr[0];
+  let min = arr[0];
+  let sum = 0;
+  let avg;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      } 
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+      sum += arr[i];
+    }
+    avg = +(sum / arr.length).toFixed(2);
+    return { min, max, avg };
 }
 
-function summElementsWorker(...arr) {
-
+// Задание 2
+function worker(arr) {
+  let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+  return sum;
 }
 
-function differenceMaxMinWorker(...arr) {
-
+function makeWork(arrOfArr, func) {
+  let max = 0;
+    for (let i = 0; i < arrOfArr.length; i++) {
+      func(arrOfArr[i]);
+        if (func(arrOfArr[i]) > max) {
+          max = func(arrOfArr[i]);
+        }
+    }
+  return max;
 }
 
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
+// Задание 3
+function worker2(arr) {
+  let max = arr[0];
+  let min = arr[0];
+  let diff = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+  return Math.abs(max - min);
 }
