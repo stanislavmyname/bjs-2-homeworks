d6c8738721441a3a2b174b206c1c22e31740b042
stanislavmyname <115893292+stanislavmyname@users.noreply.github.com>
2023-09-06 09:01:21 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks) {
+		this.marks.push(...marks)
+	}
 }
 
 Student.prototype.getAverage = function () {
-  
-}
+    if (this.marks && this.marks.length != 0) {
+			const sum = this.marks.reduce((acc, number) => acc + number, 0);
+			return  sum / this.marks.length
+		} else {
+			return  0
+		}
+	}
+
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.marks
+	delete this.subject
+	delete this.avg
+	this.excluded = reason;
 }
+	
