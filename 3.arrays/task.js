78445949fa485e9fe13e765e908e3b21e88bbb05
stanislavmyname <115893292+stanislavmyname@users.noreply.github.com>
2023-09-06 09:40:33 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
-  
+  let result;
+  result = arr1.length === arr2.length && arr1.every(function(element, index) {
+    return element === arr2[index]; 
+});
+  return result; // boolean
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+function advancedFilter(arr) {
+  let resultArr = arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number * 10);
+  return resultArr;
 }
