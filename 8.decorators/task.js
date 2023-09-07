042131185977065d1fd17d719ff059cc1e62e119
stanislavmyname <115893292+stanislavmyname@users.noreply.github.com>
2023-09-07 10:29:58 +0300

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,10 +1,45 @@
-//Задача № 1
+// //Задача № 1
+
 function cachingDecoratorNew(func) {
-  
+    const cache = [];
+    function wrapper(...args) {
+        const hash = md5(args);
+        let objInCache = cache.find((item) => item.hash === hash);
+        if (objInCache) {
+            console.log(`Из кеша: ${objInCache.value}`);
+            return `Из кеша: ${objInCache.value}`;
+        }
+
+        let result = func(...args);
+        cache.push({ hash: hash, value: result });
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+
+    }
+    return wrapper;
 }
 
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
-}
+
+// //Задача № 2
+
+function debounceDecoratorNew(fn, delay) {
+    let timeoutId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    function wrapper(...args) {
+        wrapper.allCount++;
+        if (timeoutId === null) {
+            fn.apply(this, args);
+            wrapper.count++;
+        }
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => {
+            fn.apply(this, args);
+            wrapper.count++;
+        },delay);
+    }
+    return wrapper;
 }
